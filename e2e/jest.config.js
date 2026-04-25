dc7447d4ff6895675526b7b35b68367a2f44540a
Greg Howard <greg.howard@asos.com>
2026-04-25 07:35:52 +0100

    tidy of ignore and reporter added

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -7,7 +7,15 @@ module.exports = {
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
-  reporters: ['detox/runners/jest/reporter'],
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "./e2e/report",
+      "filename": "index.html",
+      "expand": true,
+      "pageTitle": "Mile Club E2E Test Report"
+    }]
+  ],
   testEnvironment: 'detox/runners/jest/testEnvironment',
   verbose: true,
   setupFilesAfterEnv: ['<rootDir>/e2e/init.js'],
